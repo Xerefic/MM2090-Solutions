d6503f82df11ca203c3374bc7e87bae1ad20e473
Archish <archish17@gmail.com>
2021-05-26 16:13:42 +0530

    Added Comments

--- Assignment_1/question5/commands.sh
+++ Assignment_1/question5/commands.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Unzipping the chats
 tar -xvzf mm2090-apr2021-attendance.tar.gz
 mv mm2090-apr2021-attendance/ transcripts/
 
+# Grepping the name and roll number of students
 mkdir registered
 lectures=0;
 for files in `ls transcripts/`;
@@ -12,6 +14,7 @@ do
 done;
 rm -r transcripts/
 
+# Pre-processing
 for files in `ls registered/`;
 do
         cat registered/$files | sed -e 's/\(.*\) \(.*\):/\2,\1/g'  > registered/$(basename $files .txt).csv
@@ -26,6 +29,7 @@ do
 done;	
 rm -r registered/
 
+# Removing multiple instances for each lecture
 mkdir attendance
 for files in `ls cache/`;
 do
@@ -33,10 +37,13 @@ do
 done;
 rm -r cache/
 
+# Finding the total registerants
 tail -n+1 -q attendance/*.csv | awk -F, '{name[$1]=$2;}END{for (id in name){printf("%s,%s\n", id, name[id]);}}' | sort -k1 -n > registered.csv
 
+# Finding the percentage of attendance
 tail -n+1 -q attendance/*.csv | awk -F, -v total=$lectures '{data[$1]=$0;name[$1]++;}END{for (id in name){printf("%s,%.2f\%\n", data[id], name[id]/total*100);}}' | sort -k1 -n > attendance.csv
 
+# Finding absentees in every lecture
 mkdir absent
 for files in `ls attendance/`
 do
@@ -45,9 +52,11 @@ done;
 rm registered.csv
 rm -r attendance/
 
+# Processing the absentee list
 tail -n+1 -q absent/*.csv | awk -F, '{name[$1]=$2;absent[$1]=absent[$1]$3;}END{for (id in name){printf("%s,%s,%s\n", id, name[id], absent[id]);}}' | sort -k1 -n > absent.csv
 rm -r absent/
 
+# Combining the data
 awk -F, 'NR==FNR {absent[$1]=$3; next}{printf("%s,%s\n", $0, absent[$1]);}' absent.csv attendance.csv > cache.csv
 rm absent.csv
 rm attendance.csv
@@ -55,5 +64,6 @@ rm attendance.csv
 cat cache.csv | sed 's/_/ /g' > final.csv
 rm cache.csv
 
+# Adding header
 awk -F, 'BEGIN{printf("Roll,Name,Percentage,Missed Lectures\n");}{print $0;}' < final.csv > Attendance.csv
 rm final.csv
