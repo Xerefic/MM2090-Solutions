4c9bdb62417542511f8a81d04d43e00d3904134b
Archish <archish17@gmail.com>
2021-06-26 14:20:50 +0530

    Assgignment 6: Edit Question 2

--- Assignment_6/question2/movement.m
+++ Assignment_6/question2/movement.m
@@ -17,25 +17,81 @@ imwrite(state, "Iteration-0.png")
 
 # Performing the Operations
 
-N = 3*10^5;
+N = 10^6;
 for time = 1:N
     x = floor(rand()*size(state)(1));
     y = floor(rand()*size(state)(2));
-    if ((x>2)&&(x<size(state)(1)-2)&&(y>2)&&(y<size(state)(2)-2))
-        like = sum(sum(state(x-1:x+1, y-1:y+1)));
-        if (like>4*255)
-            state(x,y)=255;
-        else
-            state(x,y)=0;
+    if ((x>1)&&(x<size(state)(1))&&(y>1)&&(y<size(state)(2)))
+        like = state(x-1,y)+state(x+1,y)+state(x,y-1)+state(x,y+1);
+
+        if (like>2*255)
+            # The Like neighbours are white
+            if (state(x,y)==255)
+                state(x,y) = 0;
+                if (state(x-1,y)==0)
+                    state(x-1,y) = 255;
+                elseif (state(x+1,y)==0)
+                    state(x+1,y) = 255;
+                elseif (state(x,y-1)==0)
+                    state(x,y-1) = 255;
+                elseif (state(x,y+1)==0)
+                    state(x,y+1) = 255;
+                endif
+            endif
+        elseif (like==2*255)
+            if (state(x,y)==255)
+                state(x,y) = 0;
+                if (state(x-1,y)==0)
+                    state(x-1,y) = 255;
+                elseif (state(x+1,y)==0)
+                    state(x+1,y) = 255;
+                elseif (state(x,y-1)==0)
+                    state(x,y-1) = 255;
+                elseif (state(x,y+1)==0)
+                    state(x,y+1) = 255;
+                endif
+            elseif (state(x,y)==0)
+                # The Like neighbours are black
+                state(x,y) = 255;
+                if (state(x-1,y)==255)
+                    state(x-1,y) = 0;
+                elseif (state(x+1,y)==255)
+                    state(x+1,y) = 0;
+                elseif (state(x,y-1)==255)
+                    state(x,y-1) = 0;
+                elseif (state(x,y+1)==255)
+                    state(x,y+1) = 0;
+                endif
+            endif
+        elseif (like<2*255)
+            if (state(x,y)==0)
+                state(x,y) = 255;
+                if (state(x-1,y)==255)
+                    state(x-1,y) = 0;
+                elseif (state(x+1,y)==255)
+                    state(x+1,y) = 0;
+                elseif (state(x,y-1)==255)
+                    state(x,y-1) = 0;
+                elseif (state(x,y+1)==255)
+                    state(x,y+1) = 0;
+                endif
+            endif
+
         endif
+
     endif
     if (time<10000)
         if (mod(time,100)==0)
             name = sprintf("Iteration-%d.png", time);
             imwrite(state, name);
         endif
-    else
+    elseif (time<100000)
         if (mod(time,1000)==0)
+            name = sprintf("Iteration-%d.png", time);
+            imwrite(state, name);
+        endif
+    else
+        if (mod(time,10000)==0)
             name = sprintf("Iteration-%d.png", time);
             imwrite(state, name);
         endif
